401fae58dae586007fe605bc87ce02673a18cbc9
Alejandro Ávila <alejandro.30337890@uru.edu>
2024-11-11 13:22:01 -0400

    refactor(src): remover componente contenedor de exploración innecesario

--- src/views/home/Tab5Page.vue
+++ src/views/home/Tab5Page.vue
@@ -2,22 +2,19 @@
     <ion-page>
         <ion-header>
             <ion-toolbar>
-                <ion-title>Tab 5</ion-title>
+                <ion-title>Perfil</ion-title>
             </ion-toolbar>
         </ion-header>
         <ion-content :fullscreen="true">
             <ion-header collapse="condense">
                 <ion-toolbar>
-                    <ion-title size="large">Tab 5</ion-title>
+                    <ion-title size="large">Perfil</ion-title>
                 </ion-toolbar>
             </ion-header>
-
-            <ExploreContainer name="Tab 5 page" />
         </ion-content>
     </ion-page>
 </template>
 
 <script setup>
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
