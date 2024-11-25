a52175a943cb06efc4dc9440f4fde4add43d7b2e
Alejandro Ávila <alejandro.30337890@uru.edu>
2024-11-24 23:44:37 -0400

    feat(components-others): renderizar opción activa según ruta inicial asignada en nav-bar

--- src/components/others/NavBar.vue
+++ src/components/others/NavBar.vue
@@ -4,7 +4,7 @@
             <ion-router-outlet id="main-content" animated="false"></ion-router-outlet>
             <div class="bottom-nav">
                 <router-link v-for="(item, index) in menuWithIcons" :key="index" :to="item.to" class="nav-item"
-                    :class="{ 'active-nav-item': $route.path === item.to }">
+                    :class="{ 'active-nav-item': isActive(item.to) }">
                     <img :src="item.icon" :alt="item.label" />
                 </router-link>
             </div>
@@ -14,6 +14,7 @@
 
 <script setup>
 import { IonPage, IonTabs, IonRouterOutlet } from '@ionic/vue';
+import { useRoute } from 'vue-router';
 import menuItems from '../../utils/menu.json';
 import { iconsMap } from '../../utils/icons.js';
 
@@ -24,6 +25,18 @@ const menuWithIcons = menuItems.map(item => ({
     ...item,
     icon: iconsMap[item.icon]
 }));
+
+const route = useRoute();
+
+/**
+ * Determina si el menú debe estar activo para la ruta actual.
+ * 
+ * @param {string} path Ruta asociada al menú.
+ * @returns {boolean}
+ */
+const isActive = (path) => {
+    return route.path.startsWith(path);
+};
 </script>
 
 <style src="../../assets/menu.css"></style>
