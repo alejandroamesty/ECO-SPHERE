02fef815619037b8192de0cf642c98c4708a94a3
Alejandro Ávila <alejandro.30337890@uru.edu>
2024-11-24 23:32:18 -0400

    feat: importar y registrar nuevos íconos para pantallas

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -7,6 +7,9 @@ export const CHATS = `${VITE_SVG_ROUTE}menu/chats.svg`;
 export const PROFILE = `${VITE_SVG_ROUTE}menu/profile.svg`;
 export const SEARCH = `${VITE_SVG_ROUTE}search.svg`;
 export const NOTIFICATIONS = `${VITE_SVG_ROUTE}notifications.svg`;
+export const ARROW_LEFT = `${VITE_SVG_ROUTE}arrow-left.svg`;
+export const COMMUNITY = `${VITE_SVG_ROUTE}community.svg`;
+export const FILTER = `${VITE_SVG_ROUTE}filter.svg`;
 
 export const iconsMap = {
 	HOME,
@@ -16,4 +19,7 @@ export const iconsMap = {
 	PROFILE,
 	SEARCH,
 	NOTIFICATIONS,
+	ARROW_LEFT,
+	COMMUNITY,
+	FILTER,
 };
