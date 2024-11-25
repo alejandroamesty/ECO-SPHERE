4c0997483831e21a51ede79061b144915d7c07f1
Alejandro Ávila <alejandro.30337890@uru.edu>
2024-11-24 23:42:46 -0400

    feat(components): exportar nuevos componentes creados

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as NavBar } from "./others/NavBar.vue";
 export { default as RoundButton } from "./buttons/RoundButton.vue";
 export { default as ToggleButton } from "./buttons/ToggleButton.vue";
+export { default as ContextMenu } from "./menu/ContextMenu.vue";
+export { default as SearchInput } from "./input/SearchInput.vue";
+export { default as Breadcrumb } from "./buttons/Breadcrumb.vue";
