7a752d893ac748f11ffdc4bc94256da5e0956035
Alejandro Ávila <alejandro.30337890@uru.edu>
2024-11-24 23:39:17 -0400

    feat(components-buttons): crear componente de botón de texto para retroceder

---
+++ src/components/buttons/Breadcrumb.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="breadcrumb" @click="handleBackClick">
+        <img class="arrow-icon" :src="ARROW_LEFT" alt="Back" />
+        <span class="breadcrumb-label">{{ label }}</span>
+    </div>
+</template>
+
+<script setup>
+import { ARROW_LEFT } from '../../utils/icons';
+
+const props = defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+});
+
+const emit = defineEmits(['back']);
+
+/**
+ * Gestiona el evento click del botón de retroceso.
+ */
+const handleBackClick = () => {
+    emit('back');
+};
+</script>
+
+<style scoped>
+.breadcrumb {
+    display: flex;
+    align-items: center;
+    height: 48px;
+}
+
+.arrow-icon {
+    width: 18px;
+    height: 18px;
+    position: relative;
+}
+
+.breadcrumb-label {
+    font-family: 'Stolzl Medium';
+    font-size: 20px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    color: #FFFFFF;
+    margin-left: 9px;
+}
+</style>
