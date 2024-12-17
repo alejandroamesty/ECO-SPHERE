06dbf9c67655028f709f70a041e0db87173594d5
Alejandro Ávila <alejandro.30337890@uru.edu>
2024-12-17 10:34:10 -0400

    feat(src): evitar que apertura del teclado modifique el tamaño de la pantalla

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,16 @@
 <script setup>
 import { onMounted } from 'vue';
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { Keyboard } from '@capacitor/keyboard';
 import { StatusBar, Style } from '@capacitor/status-bar';
 
 /**
  * Configura los estilos de la barra de estado.
  */
 onMounted(async () => {
+  Keyboard.setResizeMode({
+    mode: 'none',
+  });
   await StatusBar.setStyle({ style: Style.Dark });
   await StatusBar.setBackgroundColor({
     color: '#40A578'
