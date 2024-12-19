dd6da231d0eb0d76253327663921d3823622421c
Alejandro Ávila <alejandro.30337890@uru.edu>
2024-12-18 20:14:55 -0400

    feat(components-list): crear componentes de lista de publicaciones y reportes

---
+++ src/components/list/ReportList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="report-list-container">
+        <ReportCard v-for="(report, index) in reports" :key="index" :image="report.image"
+            :description="report.description" :latitude="report.latitude" :longitude="report.longitude"
+            :date="report.date" :icon="report.icon" :name="report.name" :username="report.username" :isModal="false" />
+    </div>
+</template>
+
+<script setup>
+import { toRefs } from 'vue';
+import { ReportCard } from '..';
+
+const props = defineProps({
+    reports: {
+        type: Array,
+        required: true,
+    },
+});
+
+const { reports } = toRefs(props);
+</script>
+
+<style scoped>
+.report-list-container {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+}
+</style>
