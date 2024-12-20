329ed266d94d9c8e9f5a2f7733f27c77bf3206d4
Alejandro Ávila <alejandro.30337890@uru.edu>
2024-12-19 23:48:45 -0400

    feat(components-input): implementar captura de valor en input a través de v-model

--- src/components/input/SearchInput.vue
+++ src/components/input/SearchInput.vue
@@ -1,69 +1,87 @@
 <template>
-    <div class="search-input-container">
-        <input type="text" v-bind="$attrs" class="search-input" :placeholder="placeholder" @input="onInputChange" />
-        <RoundButton :icon="SEARCH" :size=40 :gray="true" :onClick="handleClick" />
-    </div>
+	<div class="search-input-container">
+		<input type="text" v-bind="$attrs" class="search-input" :placeholder="placeholder" v-model="inputValue" @input="onInputChange" />
+		<RoundButton :icon="SEARCH" :size="40" :gray="true" :onClick="handleClick" />
+	</div>
 </template>
 
 <script setup>
-import { defineProps, defineEmits } from 'vue';
+import { defineProps, defineEmits, ref, toRefs } from 'vue';
 import { RoundButton } from '../index';
 import { SEARCH } from '../../utils/icons';
 
 const props = defineProps({
-    placeholder: {
-        type: String,
-        required: true,
-    },
+	placeholder: {
+		type: String,
+		required: true,
+	},
+	modelValue: {
+		type: String,
+		default: '',
+	},
+	onClick: {
+		type: Function,
+		default: () => {},
+	},
 });
 
+const { placeholder, onClick } = toRefs(props);
+
 const emit = defineEmits(['update:modelValue']);
+
+const inputValue = ref(props.modelValue);
 
 /**
  * Emite el evento de cambio del valor del input.
  * @param event - Evento de cambio.
  */
-const onInputChange = (event) => {
-    emit('update:modelValue', event.target.value);
+const onInputChange = () => {
+	emit('update:modelValue', inputValue.value);
 };
 
 /**
  * Maneja el evento de clic en el botón de búsqueda.
  */
-const handleClick = () => {
-    console.log('Search button clicked');
+const handleClick = (event) => {
+	onClick.value(event);
 };
 </script>
 
 <style scoped>
 .search-input-container {
-    width: 100%;
-    height: 57px;
-    border-radius: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding-left: 20px;
-    padding-right: 8px;
-    z-index: 9999;
-    background: #EBEBEB;
-    box-shadow: 1px 1px 2px rgba(255, 255, 255, 0.3), -1px -1px 2px rgba(212, 212, 212, 0.5), inset -5px 5px 10px rgba(212, 212, 212, 0.2), inset 5px -5px 10px rgba(212, 212, 212, 0.2), inset -5px -5px 10px rgba(255, 255, 255, 0.9), inset 5px 5px 13px rgba(212, 212, 212, 0.9);
+	width: 100%;
+	height: 57px;
+	border-radius: 30px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding-left: 20px;
+	padding-right: 8px;
+	z-index: 9999;
+	background: #ebebeb;
+	box-shadow:
+		1px 1px 2px rgba(255, 255, 255, 0.3),
+		-1px -1px 2px rgba(212, 212, 212, 0.5),
+		inset -5px 5px 10px rgba(212, 212, 212, 0.2),
+		inset 5px -5px 10px rgba(212, 212, 212, 0.2),
+		inset -5px -5px 10px rgba(255, 255, 255, 0.9),
+		inset 5px 5px 13px rgba(212, 212, 212, 0.9);
 }
 
 .search-input {
-    width: 257px;
-    height: 16px;
-    border: none;
-    outline: none;
-    background: none;
-    font-family: 'Stolzl Regular', sans-serif;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 17px;
-    color: rgba(41, 43, 46, 0.5);
+	width: 257px;
+	height: 16px;
+	border: none;
+	outline: none;
+	background: none;
+	font-family: 'Stolzl Regular', sans-serif;
+	font-weight: 400;
+	font-size: 14px;
+	line-height: 17px;
+	color: rgba(41, 43, 46, 0.5);
 }
 
 .search-input::placeholder {
-    color: rgba(41, 43, 46, 0.5);
+	color: rgba(41, 43, 46, 0.5);
 }
 </style>
--- src/components/input/VerificationInput.vue
+++ src/components/input/VerificationInput.vue
@@ -1,68 +1,94 @@
 <template>
-    <div class="verification-input-container">
-        <div v-for="(digit, index) in digits" :key="index" class="digit-container">
-            <input type="text" maxlength="1" class="digit-input" v-model="digits[index]"
-                @input="handleInput(index, $event)" @focus="selectDigit($event)" />
-        </div>
-    </div>
+	<div class="verification-input-container">
+		<div v-for="(digit, index) in digits" :key="index" class="digit-container">
+			<input type="tel" maxlength="1" class="digit-input" v-model="digits[index]" @input="handleInput(index)" @keydown.backspace="handleBackspace(index, $event)" @focus="selectDigit($event)" inputmode="numeric" />
+		</div>
+	</div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 
 const emit = defineEmits(['update:code']);
 
 const digits = ref(Array(6).fill(''));
 
-const handleInput = (index, event) => {
-    const value = event.target.value;
-    digits.value[index] = value.replace(/[^0-9]/g, '').charAt(0); // Solo dígitos
-    emit('update:code', digits.value.join(''));
+watch(digits, () => {
+	emit('update:code', digits.value.join(''));
+});
 
-    if (value && index < 5) {
-        event.target.nextElementSibling?.focus();
-    }
+/**
+ * Maneja el evento de cambio de valor en un input.
+ */
+const handleInput = (index) => {
+	const currentValue = digits.value[index];
+	digits.value[index] = currentValue.replace(/[^0-9]/g, '');
+
+	if (digits.value[index] && index < digits.value.length - 1) {
+		const nextInput = document.querySelectorAll('.digit-input')[index + 1];
+		nextInput?.focus();
+	}
 };
 
+/**
+ * Maneja el evento de tecla de retroceso.
+ */
+const handleBackspace = (index, event) => {
+	if (event.key === 'Backspace' && !digits.value[index] && index > 0) {
+		const previousInput = document.querySelectorAll('.digit-input')[index - 1];
+		previousInput?.focus();
+	}
+};
+
+/**
+ * Selecciona el contenido de un input al hacer clic en él.
+ */
 const selectDigit = (event) => {
-    event.target.select();
+	event.target.select();
 };
+
+/**
+ * Enfoca el primer input al montar el componente.
+ */
+onMounted(() => {
+	const firstInput = document.querySelectorAll('.digit-input')[0];
+	firstInput?.focus();
+});
 </script>
 
 <style scoped>
 .verification-input-container {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: 9px;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	gap: 10px;
 }
 
 .digit-container {
-    width: 47.96px;
-    height: 47.96px;
-    background: #ebebeb;
-    box-shadow: 1px 1px 2px rgba(255, 255, 255, 0.3),
-        -1px -1px 2px rgba(212, 212, 212, 0.5),
-        inset -5px 5px 10px rgba(212, 212, 212, 0.2),
-        inset 5px -5px 10px rgba(212, 212, 212, 0.2),
-        inset -5px -5px 10px rgba(255, 255, 255, 0.9),
-        inset 5px 5px 13px rgba(212, 212, 212, 0.9);
-    border-radius: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+	width: 47.96px;
+	height: 47.96px;
+	background: #ebebeb;
+	box-shadow:
+		1px 1px 2px rgba(255, 255, 255, 0.3),
+		-1px -1px 2px rgba(212, 212, 212, 0.5),
+		inset -5px 5px 10px rgba(212, 212, 212, 0.2),
+		inset 5px -5px 10px rgba(212, 212, 212, 0.2),
+		inset -5px -5px 10px rgba(255, 255, 255, 0.9),
+		inset 5px 5px 13px rgba(212, 212, 212, 0.9);
+	border-radius: 20px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 
 .digit-input {
-    width: 14px;
-    height: 20px;
-    font-family: 'Stolzl Regular';
-    font-size: 20px;
-    color: #292b2e;
-    background: none;
-    border: none;
-    outline: none;
-    text-align: center;
+	font-family: 'Stolzl Regular', sans-serif;
+	font-size: 20px;
+	color: #292b2e;
+	background: none;
+	border: none;
+	outline: none;
+	text-align: center;
 }
 </style>
