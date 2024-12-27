e18844712d0b76a259e38c7e8f7f1fcdc1f07a31
Alejandro Ávila <alejandro.30337890@uru.edu>
2024-12-27 16:59:23 -0400

    feat(stores): agregar función para autenticar token del usuario

--- src/stores/globalStore.js
+++ src/stores/globalStore.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia';
+import { jwtDecode } from 'jwt-decode';
 import { ref } from 'vue';
 
 export const useGlobalStore = defineStore(
@@ -11,17 +12,30 @@ export const useGlobalStore = defineStore(
 			token.value = newToken;
 		};
 
+		const isAuthenticated = () => {
+			if (!token.value) {
+				return false;
+			}
+
+			const decoded = jwtDecode(token.value);
+			const now = Date.now() / 1000;
+
+			return decoded.exp > now;
+		};
+
 		const setUser = (newUser) => {
 			user.value = newUser;
 		};
 
 		const $reset = () => {
 			token.value = null;
+			user.value = null;
 		};
 
 		return {
 			token,
 			setToken,
+			isAuthenticated,
 			user,
 			setUser,
 			$reset,
